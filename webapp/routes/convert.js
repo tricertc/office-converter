c058b2021ebd449765bb7fe3f904035ce5381d39
Tim Rice <timrice@gmail.com>
2018-01-19 16:06:16 -0500

    Added ability to secure pdfs.

--- webapp/routes/convert.js
+++ webapp/routes/convert.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const converter = require('../utils/converter');
+const qpdf = require('node-qpdf');
 const path = require('path');
 const fs = require('fs');
 
@@ -9,8 +10,14 @@ router.post('/', async (req, res) => {
   let ext = path.extname(document.name);
   let base = path.basename(document.name, ext);
 
+
   try {
     let pdfFile = await converter.convert(document.data, document.name);
+
+    if (req.body.protect) {
+      await converter.protect(pdfFile, req.body.password);
+    }
+
     res.download(pdfFile, `${base}.pdf`, (err) => {
       fs.unlinkSync(pdfFile);
     });
--- webapp/utils/converter.js
+++ webapp/utils/converter.js
@@ -1,8 +1,10 @@
 const request = require('request');
 const config = require('../config');
 const uuid = require('uuid/v1');
+const qpdf = require('node-qpdf');
 const path = require('path');
 const fs = require('fs');
+const mv = require('mv');
 
 exports.convert = (buffer, name) => {
   return new Promise((resolve, reject) => {
@@ -33,5 +35,31 @@ exports.convert = (buffer, name) => {
     }).pipe(output);
 
     output.on('finish', () => resolve(tmpFile));
+  });
+};
+
+exports.protect = (pdfFile, password) => {
+  return new Promise((resolve, reject) => {
+    try {
+      let tmpFile = path.join(config.UPLOADS_DIR, uuid());
+      mv(pdfFile, tmpFile, () => {
+        let options = {
+          keyLength: 128,
+          password: password
+        };
+
+        let stream = fs.createWriteStream(pdfFile);
+
+        qpdf.encrypt(tmpFile, options).pipe(stream);
+
+        stream.on('finish', () => {
+          resolve();
+          fs.unlinkSync(tmpFile);
+        });
+      });
+    }
+    catch (ex) {
+      reject(ex);
+    }
   });
 };
